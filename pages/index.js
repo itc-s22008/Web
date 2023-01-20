1c12f1cbd46264a53c5d1708bbd739bb1466a1f3
itc-s22008 <s22008@std.it-college.ac.jp>
2023-01-20 01:01:19 +0000

    課題です。

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+export default function Home () {
+  return (
+    <>
+      <Link href='/sample' legacyBehavior>
+        <a> Sample Website  </a>
+      </Link>
+
+      <Link href='sample2' legacyBehavior>
+        <a> 新聞の1ページ</a> 
+      </Link>
+
+      <Link href='sample3' legacyBehavior>
+        <a>ブログ記事一覧 </a>
+      </Link>
+
+      <Link href='sample4' legacyBehavior>
+        <a> 三段記事の一覧 </a>
+      </Link>
+
+      <Link href='sample5' legacyBehavior>
+        <a> グリッドレイアウト </a>
+      </Link>
+
+    </>
+  )
+}
---
+++ pages/sample.js
@@ -0,0 +1,6 @@
+import styles from 'styles/sample.module.css'
+export default function test () {
+  return (
+    <p> first </p>
+  )
+}
