2bb4b8a12e001b211dbb489fca16a76542746966
itc-s22008 <s22008@std.it-college.ac.jp>
2023-01-20 01:46:43 +0000

    更新しました。

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,19 @@ export default function Home () {
         <a> Sample Website  </a>
       </Link>
 
-      <Link href='sample2' legacyBehavior>
-        <a> 新聞の1ページ</a> 
+      <Link href='/sample2' legacyBehavior>
+        <a> page </a>
       </Link>
 
-      <Link href='sample3' legacyBehavior>
+      <Link href='/sample3' legacyBehavior>
         <a>ブログ記事一覧 </a>
       </Link>
 
-      <Link href='sample4' legacyBehavior>
+      <Link href='/sample4' legacyBehavior>
         <a> 三段記事の一覧 </a>
       </Link>
 
-      <Link href='sample5' legacyBehavior>
+      <Link href='/sample5' legacyBehavior>
         <a> グリッドレイアウト </a>
       </Link>
 
--- pages/sample.js
+++ pages/sample.js
@@ -1,4 +1,3 @@
-import styles from 'styles/sample.module.css'
 export default function test () {
   return (
     <p> first </p>
